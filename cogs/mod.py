94a4c659353e3ec14f9b96598bc865d3853801a3
Daftscientist <67394753+Daftscientist@users.noreply.github.com>
2020-09-16 21:25:03 +0100

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -4,8 +4,8 @@ from discord.ext import commands
 
 class Mod(commands.Cog):
 
-    def __init__(self, client):
-        self.client = client
+    def __init__(self, bot):
+        self.bot = bot
 
 
     # this is the clear command
@@ -19,21 +19,17 @@ class Mod(commands.Cog):
     @commands.has_permissions(ban_members = True)
     async def ban(self, ctx, member : discord.Member=None, *, reason = None):
         if member is None:
-            em = discord.Embed(title = 'Please specify a member.',colour = discord.Colour.orange())
-            em.set_footer(text="Coded by daftscientist#8570 | CereBro | User ID: " + str(ctx.message.author.id))
+            em = discord.Embed(title = 'Please specify a member.')
             await ctx.send(embed = em)
             return
         await member.ban(reason = reason)
-        em = discord.Embed(title = f'You banned {member}',colour = discord.Colour.orange())
-        em.set_footer(text="Coded by daftscientist#8570 | CereBro | User ID: " + str(ctx.message.author.id))
+        em = discord.Embed(title = f'You banned {member}')
         await ctx.send(embed = em)
+        
+        embed = discord.Embed(title='You have been banned from The Coding Community', description=f'Banned by {member}')
+        embed.add_field(name='Reason:', value=f'{reason}')
+        await member.send(embed=embed)
 
-    @ban.error
-    async def ban_error(self, ctx, error):
-        if isinstance(error, commands.CheckFailure):
-            em = discord.Embed(title = 'Nice try but you are not allowed to ban people',colour = discord.Colour.orange())
-            em.set_footer(text="Coded by daftscientist#8570 | CereBro | User ID: " + str(ctx.message.author.id))
-            await ctx.send(embed = em)
 
     @commands.command()
     @commands.has_permissions(ban_members = True)
@@ -46,79 +42,59 @@ class Mod(commands.Cog):
 
             if (user.name, user.discriminator) == (member_name, member_discriminator):
                 await ctx.guild.unban(user)
-                em = discord.Embed(title = f'You Unbanned {user.mention}',colour = discord.Colour.orange())
-                em.set_footer(text="Coded by daftscientist#8570 | CereBro | User ID: " + str(ctx.message.author.id))
+                em = discord.Embed(title = f'You Unbanned {user.mention}')
                 await ctx.send(embed = em)
                 return            
 
-    @unban.error
-    async def unban_error(self, ctx, error):
-        if isinstance(error, commands.CheckFailure):
-            em = discord.Embed(title = 'Nice try but you are not allowed to unban people',colour = discord.Colour.orange())
-            em.set_footer(text="Coded by daftscientist#8570 | CereBro | User ID: " + str(ctx.message.author.id))
-            await ctx.send(embed = em)
+
 
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def kick(self, ctx, member:discord.Member = None):
         if not member:
-            em = discord.Embed(title = 'Please specify a member.',colour = discord.Colour.orange())
-            em.set_footer(text="Coded by daftscientist#8570 | CereBro | User ID: " + str(ctx.message.author.id))
+            em = discord.Embed(title = 'Please specify a member.')
             await ctx.send(embed = em)
             return
         await member.kick()
-        em = discord.Embed(title = f'You kicked {member}',colour = discord.Colour.orange())
-        em.set_footer(text="Coded by daftscientist#8570 | CereBro | User ID: " + str(ctx.message.author.id))
+        em = discord.Embed(title = f'You kicked {member}')
         await ctx.send(embed = em)
-    @kick.error
-    async def kick_error(self, ctx, error):
-        if isinstance(error, commands.CheckFailure):
-            em = discord.Embed(title = 'Nice try but you are not allowed to kick people',colour = discord.Colour.orange())
-            em.set_footer(text="Coded by daftscientist#8570 | CereBro | User ID: " + str(ctx.message.author.id))
-            await ctx.send(embed = em)
+        embed = discord.Embed(title='You have been kicked from The Coding Community', description=f'Kicked by {member}')
+        embed.add_field(name='Reason:', value=f'{reason}')
+        await member.send(embed=embed)
+
 
     #mute command
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def mute(self, ctx, member: discord.Member=None):
         if not member:
-            em = discord.Embed(title = 'Please specify a member.',colour = discord.Colour.orange())
-            em.set_footer(text="Coded by daftscientist#8570 | CereBro | User ID: " + str(ctx.message.author.id))
+            em = discord.Embed(title = 'Please specify a member.')
             await ctx.send(embed = em)
             return
         role = discord.utils.get(ctx.guild.roles, name="Muted")
         await member.add_roles(role)
-        em = discord.Embed(title = f'{member.mention} got Got muted',colour = discord.Colour.orange())
-        em.set_footer(text="Coded by daftscientist#8570 | CereBro | User ID: " + str(ctx.message.author.id))
+        em = discord.Embed(title = f'You muted{member.mention}')
         await ctx.send(embed = em)
+        embed = discord.Embed(title='You have been muted in The Coding Community', description=f'Muted by {member}')
+        embed.add_field(name='Reason:', value=f'{reason}')
+        await member.send(embed=embed)
 
-    @mute.error
-    async def mute_error(self, ctx, error):
-        if isinstance(error, commands.CheckFailure):
-            em = discord.Embed(title = 'Nice try but you are not allowed to mute people',colour = discord.Colour.orange())
-            em.set_footer(text="Coded by daftscientist#8570 | CereBro | User ID: " + str(ctx.message.author.id))
-            await ctx.send(embed = em)
 
     #unmute command
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def unmute(self, ctx, member: discord.Member=None):
         if not member:
-            em = discord.Embed(title = 'Please specify a member.',colour = discord.Colour.orange())
-            em.set_footer(text="Coded by daftscientist#8570 | CereBro | User ID: " + str(ctx.message.author.id))
+            em = discord.Embed(title = 'Please specify a member.')
             await ctx.send(embed = em)
             return
         role = discord.utils.get(ctx.guild.roles, name="Muted")
         await member.remove_roles(role)
-        em = discord.Embed(title = f'{member.mention} got unmuted',colour = discord.Colour.orange())
-        em.set_footer(text="Coded by daftscientist#8570 | CereBro | User ID: " + str(ctx.message.author.id))
+        em = discord.Embed(title = f'{member.mention} got unmuted')
         await ctx.send(embed = em)
-    @mute.error
-    async def unmute_error(self, ctx, error):
-        if isinstance(error, commands.CheckFailure):
-            em = discord.Embed(title = 'Nice try but you are not allowed to unmute people',colour = discord.Colour.orange())
-            em.set_footer(text="Coded by daftscientist#8570 | CereBro | User ID: " + str(ctx.message.author.id))
-            await ctx.send(embed = em)
+        embed = discord.Embed(title='You have been unmuted in The Coding Community', description=f'Unmuted by {member}')
+        embed.add_field(name='Reason:', value=f'{reason}')
+        await member.send(embed=embed)
 
-def setup(client):
-    client.add_cog(Mod(client))
+def setup(bot):
+    bot.add_cog(Mod(bot))
