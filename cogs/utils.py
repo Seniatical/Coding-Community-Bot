7fccec914222914a1b1f4801967edeca60b7dbe1
capslock321 <69331371+capslock321@users.noreply.github.com>
2020-09-16 21:22:04 -0400

    Update utils.py

--- cogs/utils.py
+++ cogs/utils.py
@@ -1,9 +1,18 @@
 import discord
 from discord.ext import commands
 
-class Utility(commands.Cog, name = 'util'):
+class Utility(commands.Cog):
     
     def __init__ (self,bot):
         self.bot = bot
 
-      
+    @commands.command()
+    async def ping(self, ctx):
+        before = time.monotonic()
+        before_ws = int(round(self.bot.latency * 1000, 1))
+        message = await ctx.send(":ping_pong: Pong")
+        ping = (time.monotonic() - before) * 1000
+        await message.edit(content=f":ping_pong: WS: {before_ws}ms  |  REST: {int(ping)}ms")
+        
+def setup(bot):
+    bot.add_cog(Utility(bot))
