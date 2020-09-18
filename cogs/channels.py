216cb929887540b8687120a97697d0c651186e6a
capslock321 <69331371+capslock321@users.noreply.github.com>
2020-09-18 14:58:53 -0400

    maybe fixed the time out thingy
    i love making a python server knockoff

--- cogs/channels.py
+++ cogs/channels.py
@@ -13,7 +13,7 @@ class Help(commands.Cog, name = 'help'):
     @commands.Cog.listener()
     async def on_message(self,message):
         cata = self.bot.get_channel(756195945854795927)
-        back = self.bot.get_channel(755862487999447063)
+        back = self.bot.get_channel(754710748353265745)
         role = discord.utils.get(message.guild.roles, name = 'Helper')
         if message.channel.id == 754710893538836480 and message.author.id != 755479510387916871:
             if self.bot.hp1 == True:
