e702b08ff15b764c35d35d262bc9d97a9d5c0bf8
capslock321 <69331371+capslock321@users.noreply.github.com>
2020-09-18 09:36:26 -0400

    Update channels.py

--- cogs/channels.py
+++ cogs/channels.py
@@ -14,6 +14,7 @@ class Help(commands.Cog, name = 'help'):
     async def on_message(self,message):
         cata = self.bot.get_channel(756195945854795927)
         back = self.bot.get_channel(755862487999447063)
+        role = discord.utils.get(message.guild.roles, name = 'Helper')
         if message.author.guild_permissions.manage_messages == True:
             pass
         elif message.channel.id == 754710893538836480 and message.author.id != 755479510387916871:
@@ -23,7 +24,7 @@ class Help(commands.Cog, name = 'help'):
                 embed = discord.Embed(title = f'You have just claimed {message.channel.name}.',
                  description = 'You would not be able to claim any other help channels for 15 minutes.', color = discord.Colour.from_rgb(255,221,170))
                 embed.add_field(name = f'When asking questions: ', value = "Don't just say `i need help`, just state your question and hopefully someone comes to help.")
-                embed.add_field(name = f'If you need someones attention: ', value = "Ping <@!726650418444107869>, they would be happy to help.")
+                embed.add_field(name = f'If you need someones attention: ', value = f"Ping {role.mention}, they would be happy to help.")
                 await message.channel.send(embed = embed)
                 cool = discord.utils.get(message.guild.roles, name = 'Cooldown')
                 await message.author.add_roles(cool)
@@ -35,6 +36,7 @@ class Help(commands.Cog, name = 'help'):
                 embed2 = discord.Embed(title = 'This channel was closed due to inactivity.',
                 description = 'If you still have problems, just claim another help channel.', color = discord.Colour.from_rgb(255,221,170))
                 await message.channel.send(embed = embed2)
+                self.bot.hp1 = False
         elif message.channel.id == 754710998769991680 and message.author.id != 755479510387916871:
             if self.bot.hp2 == True:
                 pass
@@ -42,7 +44,7 @@ class Help(commands.Cog, name = 'help'):
                 embed = discord.Embed(title = f'You have just claimed {message.channel.name}.',
                  description = 'You would not be able to claim any other help channels for 15 minutes.', color = discord.Colour.from_rgb(255,221,170))
                 embed.add_field(name = f'When asking questions: ', value = "Don't just say `i need help`, just state your question and hopefully someone comes to help.")
-                embed.add_field(name = f'If you need someones attention: ', value = "Ping <@!726650418444107869>, they would be happy to help.")
+                embed.add_field(name = f'If you need someones attention: ', value = f"Ping {role.mention}, they would be happy to help.")
                 await message.channel.send(embed = embed)
                 cool = discord.utils.get(message.guild.roles, name = 'Cooldown')
                 await message.author.add_roles(cool)
@@ -54,7 +56,7 @@ class Help(commands.Cog, name = 'help'):
                 embed2 = discord.Embed(title = 'This channel was closed due to inactivity.',
                 description = 'If you still have problems, just claim another help channel.', color = discord.Colour.from_rgb(255,221,170))
                 await message.channel.send(embed = embed2)
-                
+                self.bot.hp2 = False
         elif message.channel.id == 754711103665078273 and message.author.id != 7755479510387916871:
             if self.bot.hp3 == True:
                 pass
@@ -62,7 +64,7 @@ class Help(commands.Cog, name = 'help'):
                 embed = discord.Embed(title = f'You have just claimed {message.channel.name}.',
                  description = 'You would not be able to claim any other help channels for 15 minutes.', color = discord.Colour.from_rgb(255,221,170))
                 embed.add_field(name = f'When asking questions: ', value = "Don't just say `i need help`, just state your question and hopefully someone comes to help.")
-                embed.add_field(name = f'If you need someones attention: ', value = "Ping <@!726650418444107869>, they would be happy to help.")
+                embed.add_field(name = f'If you need someones attention: ', value = f"Ping {role.mention}, they would be happy to help.")
                 await message.channel.send(embed = embed)
                 cool = discord.utils.get(message.guild.roles, name = 'Cooldown')
                 await message.author.add_roles(cool)
@@ -74,7 +76,7 @@ class Help(commands.Cog, name = 'help'):
                 embed2 = discord.Embed(title = 'This channel was closed due to inactivity.',
                 description = 'If you still have problems, just claim another help channel.', color = discord.Colour.from_rgb(255,221,170))
                 await message.channel.send(embed = embed2)
-
+                self.bot.hp3 = False
     @commands.command()
     async def close(self,ctx):
         cata = self.bot.get_channel(754710748353265745)
