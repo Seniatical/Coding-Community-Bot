ef33ad03b291bea65849b4f4d19016c108633464
JakeLion <69877521+JakeLion@users.noreply.github.com>
2020-09-17 18:08:18 -0600

    Update info.py
    This command creates an embed, title has the users username, description has the users suggestion. If the user fails to input a suggestion the bot will detect this and remind them of it. The command that executes this is also deleted right away because there is no purpose in putting the suggestion in the suggestion channel if it sits in the bot command channel. 
    This command also adds ✅ and ❎ to the suggestion so that users can easily vote.
    Finally, in order to ensure anyone can make a suggestion, the suggestion command/syntax is listed in the footer, allowing users to see how to use the command.
    
    --Jake.#8428 / JakeLion

--- cogs/info.py
+++ cogs/info.py
@@ -15,19 +15,7 @@ class Info(commands.Cog):
         embed=discord.Embed(title=f'{avamember} avatar!!')
         embed.set_image(url=userAvatarUrl)
         await ctx.send(embed=embed)
-        
-    @commands.command(aliases = ['info'])
-    async def whois(self,ctx, member:discord.Member):
-        roles = [role for role in member.roles[1:]]
-        embed = discord.Embed(title = str(member) + "'s profile:", description = 'Requested by: ' + str(ctx.author),color = discord.Colour.from_rgb(54,151,255))
-        embed.set_image(url = str(member.avatar_url))
-        embed.add_field(name = 'Member joined guild on:', value = str(member.joined_at.strftime("%a, %d %B %Y, %I:%M %p UTC")), inline = True)
-        embed.add_field(name = 'Member joined discord on:', value = str(member.created_at.strftime("%a, %d %B %Y, %I:%M %p UTC")),inline = True)
-        embed.add_field(name = "Member's nickname:", value = str(member.nick))
-        embed.add_field(name = "Member's roles: ", value=" ".join([role.mention for role in roles]), inline = True)
-        embed.add_field(name = "Member's top role:", value = str(member.top_role.mention),inline = True)
-        embed.add_field(name = "Member's id:", value = str(member.id))
-        await ctx.send(embed = embed)
+
 
     @commands.command(aliases = ['guild'])
     async def server(self, ctx):
@@ -42,6 +30,19 @@ class Info(commands.Cog):
             embed.add_field(name="Bots", value=findbots, inline=True)
             embed.add_field(name = "Guild created at: ", value = str(ctx.guild.created_at.strftime("%a, %d %B %Y, %I:%M %p UTC")))
             await ctx.send(embed =  embed)
+            
+    @commands.command(aliases=['idea', 'tell', 'suggestion'])
+    async def suggest(self, ctx, *, suggestion = None):
+        channel = bot.get_channel(729711188392280154)
+        if suggestion == None:
+            await ctx.send('Please follow this format:\n`>suggest **Your suggestion**`')
+        else:
+            em = discord.Embed(title=f'Suggestion by {ctx.author.name}', description=suggestion, color=discord.Color(0xf58742))
+            em.set_footer(text='Have an idea? use `>suggest` in a bot command channel!')
+            await ctx.message.delete()
+            msg = channel.send(embed=em)
+            msg.add_reaction('\U00002705')
+            msg.add_reaction('\u274E')
 
 
 
