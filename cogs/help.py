649edf6a54297d7e299aa449070e933722f0d838
Daftscientist <67394753+Daftscientist@users.noreply.github.com>
2020-09-19 05:35:48 +0100

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -21,13 +21,13 @@ class Help(commands.Cog):
               await ctx.send(embed=embed)
 
   @commands.command()
-  async def mod(self, ctx):
-              embed=discord.Embed(title=':hammer_pick: Moderation Commands :hammer_pick:', description='\n\n`>clear` - This command clears a spesified  ammount of messages from a text channel\n`>mute` - Mutes the spesified player\n`>unmute` - This command unmutes a user.\n`>kick` - Kicks a spesified user\n`>ban` - This command bans a user.\n`>unban` - This command bans a user.\n`>lockdown` - This locksdown a certain channel.\n`>warn` - This warns the user. \n`>removewarn` - This removes a warn.\n`>warns` - This shows warns. ')
+  async def helpmod(self, ctx):
+              embed=discord.Embed(title=':hammer_pick: Moderation Commands :hammer_pick:', description='\n\n`>clear` - This command clears a spesified  ammount of messages from a text channel\n`>mute` - Mutes the spesified player\n`>unmute` - This command unmutes a user.\n`>kick` - Kicks a spesified user\n`>ban` - This command bans a user.\n`>unban` - This command bans a user.\n`>lockdown` - This locksdown a certain channel.\n`>warn` - This warns the user. \n`>removewarn` - This removes a warn.\n`>warns` - This shows warns.\n`>lock` - locks a channel\n`>unlock` - unlocks a channel\n`>slowmode` - sets channel slowmode')
               await ctx.send(embed=embed)
 
   #fun help command
   @commands.command()
-  async def fun(self, ctx):
+  async def helpfun(self, ctx):
               embed=discord.Embed(title=':zany_face: Fun Commands :zany_face:', description='\n\n`>8ball` - This command you say _8ball then ask your question!\n`>whisper` - lets you send a dm to someone.\n`>camel` - lets you camelfy a word!\n`>reverse` - lets you reverse a word!\n`>password` - generates you a random password\n`>sayname` - says the users name!')
               await ctx.send(embed=embed)
    
@@ -41,3 +41,15 @@ class Help(commands.Cog):
    async def helprandom(self, ctx)
               embed=discord.Embed(title='coming soon!')
               await ctx.send(embed=embed)
+   
+
+    @commands.command()
+    async def helpcurrency(ctx):
+                embed=discord.Embed(color=discord.Colour.orange())
+                embed=discord.Embed(title=':money_with_wings: Currency Commands :money_with_wings:', description='\n\n`?bag` - Shows you your inventory\n`?balance` - Shows you your balence!\n`?beg` - lets you ask for money!\n`?work` - lets work for money!\n`?buy` - lets you buy somthing!\n`?dep` - lets you put money from wallet to bank!\n`?rob` - lets you steal from someone!\n`?send` - lets you send someone money\n`?shop` - lets you see availible products\n`?slots` - lets you gamble\n`?with` - lets you take money from bank and put it in wallet!\n`?leaderboard` - lets see the highest people!\n`?sell` - lets you sell an object!')
+                await ctx.send(embed=embed)
+   
+ 
+
+
+
