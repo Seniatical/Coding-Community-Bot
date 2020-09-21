9e586765970e4f4d7044f5306d7eda0e73789ca7
capslock321 <69331371+capslock321@users.noreply.github.com>
2020-09-20 20:51:46 -0400

    in memory of dalek

--- cogs/dalek.py
+++ cogs/dalek.py
@@ -1,1 +1,19 @@
+import discord
+from discord.ext import commands
 
+class Dalek(commands.Cog):
+    
+    def __init__(self,bot):
+        self.bot = bot
+        
+    @commands.command()
+    async def amiadmin(self,ctx):
+        if ctx.author.guild_permissions.administrator == True:
+            await ctx.send(f'Yes {ctx.author.name} you are an admin! :white_check_mark:')
+        elif ctx.author.id == 579326961038524446:
+            await ctx.send('Yes Happy Days you are an admin! :white_check_mark:')
+        else:
+            await ctx.send(f'no, heck off {ctx.author.name}')
+            
+def setup(bot):
+    bot.add_cog(Dalek(bot))
