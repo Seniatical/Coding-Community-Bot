c6d0ffd8c33a6b91d157935b18d759aee00f310e
capslock321 <69331371+capslock321@users.noreply.github.com>
2020-09-26 12:43:03 -0400

    fixed annoying bug

--- cogs/mod.py
+++ cogs/mod.py
@@ -65,6 +65,7 @@ class Mod(commands.Cog):
         embed = discord.Embed(title = f'Successfully softbanned {member}')
         await ctx.send(embed = embed)
     
+    
     @commands.command()
     async def tempban(self,ctx,member:discord.Member,time,*,reason = 'No Reason Provided'):
         with open('guild.json','r') as f:
@@ -91,23 +92,13 @@ class Mod(commands.Cog):
             await asyncio.sleep(int(time))
         elif indicator == 'd':
             await asyncio.sleep(int(time) * 86400)
-        await member.unban(reason = 'Timer has expired.')      
-            
-            
-            
-            
-            
-            
-        
- 
+        await member.unban(reason = 'Timer has expired.')     
 
-    # this is the clear command
     @commands.command(aliases = ['purge'])
     @commands.has_permissions(manage_messages=True)
     async def clear(self,ctx,amount:int = 5):
         await ctx.channel.purge(limit=amount)
 
-    #The below code bans player.
     @commands.command()
     @commands.has_permissions(ban_members = True)
     async def ban(self, ctx, member : discord.Member=None, *, reason = None):
@@ -143,7 +134,7 @@ class Mod(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(administrator=True)
-    async def kick(self, ctx, member:discord.Member = None):
+    async def kick(self, ctx, member:discord.Member = None,*,reason = 'No reason provided'):
         if not member:
             em = discord.Embed(title = 'Please specify a member.')
             await ctx.send(embed = em)
@@ -166,7 +157,7 @@ class Mod(commands.Cog):
                         await ctx.send(embed = em)
                         return
                     if member == ctx.message.author:
-                        embed = discord.Embed(title = 'You may not mute yourself.')
+                        embed = discord.Embed(title = 'No you may not mute yourself.')
                         await ctx.send(embed = embed)
                         break
                     else:
@@ -178,11 +169,8 @@ class Mod(commands.Cog):
                             break
                         muted_give = discord.utils.get(ctx.guild.roles, name = 'Muted')
                         await member.add_roles(muted_give)
-                        em = discord.Embed(title = f'You muted{member.mention}')
-                        await ctx.send(embed = em)
-                        embed = discord.Embed(title='You have been muted in The Coding Community', description=f'Muted by {member}')
-                        embed.add_field(name='Reason:', value=f'{reason}')
-                        await member.send(embed=embed)
+                        embed = discord.Embed(title = str(member) + ' was successfully muted by: ' + str(ctx.author) + ' for ' + str(time) + ' Reason:' + str(reason))
+                        await ctx.send(embed = embed)
                         time = time[:-1]
                         if indicator == 'm':
                             await asyncio.sleep(int(time) * 60)
@@ -199,67 +187,58 @@ class Mod(commands.Cog):
                 except AttributeError:
                     muted = await ctx.guild.create_role(name = "Muted")
                     await ctx.message.channel.set_permissions(ctx.guild.get_role(muted.id), send_messages = False)
-                    
-   @commands.command()
-   async def tempban(self,ctx,member:discord.Member,time,*,reason = 'No Reason Provided'):
-       with open('guild.json','r') as f:
-           channels = json.load(f)
-       indicator = time[-1]
-       if indicator == "m" or indicator == "s" or indicator == 'h' or indicator == 'd':
-           pass
-       else:
-           await ctx.send('Incorrect Time Format.')
-       embed = discord.Embed(title = f'{member} has been temp banned for {time}.')
-       await ctx.send(embed = embed)
-       await member.ban(reason = reason)
-       chanid = channels[str(ctx.guild.id)]['admin'][0]['adid']
-       channel = self.bot.get_channel(chanid)
-       embed = discord.Embed(title = 'Member Tempbanned!', color = discord.Colour.red())
-       embed.add_field(name = f'{member} was tempbanned({time}) from {ctx.guild.name} for: ',value = f'{reason}')
-       await channel.send(embed = embed)
-       time = time[:-1]
-       if indicator == 'm':
-           await asyncio.sleep(int(time) * 60)
-       elif indicator == 'h':
-           await asyncio.sleep(int(time) * 3600)
-       elif indicator == 's':
-           await asyncio.sleep(int(time))
-       elif indicator == 'd':
-           await asyncio.sleep(int(time) * 86400)
-       await member.unban(reason = 'Timer has expired.')
+        except Exception:
+            traceback.print_exc()
 
-    #mute command
     @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def mute(self, ctx, member: discord.Member=None):
-        if not member:
-            em = discord.Embed(title = 'Please specify a member.')
-            await ctx.send(embed = em)
-            return
-        role = discord.utils.get(ctx.guild.roles, name="Muted")
-        await member.add_roles(role)
-        em = discord.Embed(title = f'You muted{member.mention}')
-        await ctx.send(embed = em)
-        embed = discord.Embed(title='You have been muted in The Coding Community', description=f'Muted by {member}')
-        embed.add_field(name='Reason:', value=f'{reason}')
-        await member.send(embed=embed)
-
-
-    #unmute command
+    @commands.has_permissions(manage_guild=True)
+    async def warn(self,ctx , member : discord.Member ,* , reason = "No reason Provided"):
+        with open('warnings.json','r') as f:
+            warns = json.load(f)
+        if str(ctx.guild.id) not in warns:
+            warns[str(ctx.guild.id)] = {}
+        if str(member.id) not in warns[str(ctx.guild.id)]:
+            warns[str(ctx.guild.id)][str(member.id)] = {}
+            warns[str(ctx.guild.id)][str(member.id)]["warns"] = 1
+            warns[str(ctx.guild.id)][str(member.id)]["warnings"] = [reason]
+        else:
+            warns[str(ctx.guild.id)][str(member.id)]["warnings"].append(reason)
+        with open('warnings.json','w') as f:
+            json.dump(warns , f)
+            await ctx.send(f"{member.mention} was warned for: {reason}")
+            
+            embed = discord.Embed(title='You have been warned in The Coding Community', description=f'You received a warning from {member}')
+            embed.add_field(name='Reason:', value=f'{reason}')
+            await member.send(embed=embed)
+            
     @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def unmute(self, ctx, member: discord.Member=None):
-        if not member:
-            em = discord.Embed(title = 'Please specify a member.')
-            await ctx.send(embed = em)
-            return
-        role = discord.utils.get(ctx.guild.roles, name="Muted")
-        await member.remove_roles(role)
-        em = discord.Embed(title = f'{member.mention} got unmuted')
-        await ctx.send(embed = em)
-        embed = discord.Embed(title='You have been unmuted in The Coding Community', description=f'Unmuted by {member}')
-        embed.add_field(name='Reason:', value=f'{reason}')
-        await member.send(embed=embed)
+    @commands.has_permissions(manage_guild=True)
+    async def removewarn(self,ctx, member: discord.Member, num: int, *, reason='No reason provided.'):
+        with open('warnings.json' , 'r') as f:
+            warns = json.load(f)
+
+        num -= 1
+        warns[str(ctx.guild.id)][str(member.id)]["warns"] -= 1
+        warns[str(ctx.guild.id)][str(member.id)]["warnings"].pop(num)
+        with open('warnings.json' , 'w') as f:
+            json.dump(warns , f)
+            await ctx.send('Warn has been removed!')
+            embed = discord.Embed(title='Your warn in The Coding Community has been removed', description=f'Your warning was removed by {ctx.author}')
+            await member.send(embed=embed)
+
+            
+    @commands.command()
+    @commands.has_permissions(manage_guild=True)
+    async def warns(self,ctx , member : discord.Member):
+        with open('warnings.json', 'r') as f:
+            warns = json.load(f)
+
+        num = 1
+        warnings = discord.Embed(title = f"{member}\'s warns")
+        for warn in warns[str(ctx.guild.id)][str(member.id)]["warnings"]:
+            warnings.add_field(name = f"Warn {num}" , value = warn)
+            num += 1
+        await ctx.send(embed = warnings)
 
 def setup(bot):
     bot.add_cog(Mod(bot))
